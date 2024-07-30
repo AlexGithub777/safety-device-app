7548f9de0ac29de56201d161cc4c3cba310b7645
Alex <alexscott200020@gmail.com>
2024-07-31 05:42:16 +1200

    add logging

--- internal/database/database.go
+++ internal/database/database.go
@@ -3,6 +3,7 @@ package database
 import (
 	"database/sql"
 	"fmt"
+	"log"
 
 	"github.com/AlexGithub777/safety-device-app/internal/config"
 	_ "github.com/lib/pq" // PostgreSQL driver
@@ -13,12 +14,20 @@ type DB struct {
 }
 
 func NewDB(cfg config.Config) (*DB, error) {
+	log.Println("Connecting to database...")
 	connStr := fmt.Sprintf("user=%s password=%s dbname=%s host=%s port=%d sslmode=disable",
 		cfg.DBUser, cfg.DBPassword, cfg.DBName, cfg.DBHost, cfg.DBPort)
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		return nil, err
 	}
+
+	// Ping the database to ensure connection is established
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	log.Println("Database connected successfully")
 
 	return &DB{db}, nil
 }
