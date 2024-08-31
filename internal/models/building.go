7c5176f25ac27648e9b3dad2c579b0908973d42b
Alex <alexscott200020@gmail.com>
2024-08-31 23:57:26 +1200

    updated admin page

--- internal/models/building.go
+++ internal/models/building.go
@@ -2,7 +2,8 @@ package models
 
 // BuildingT represents the buildings in each site
 type Building struct {
-	BuildingID  int    `json:"building_id"`
-	SiteID      int    `json:"site_id"`
+	BuildingID   int    `json:"building_id"`
+	SiteID       int    `json:"site_id"`
 	BuildingCode string `json:"building_code"`
+	SiteName     string `json:"site_name"`
 }
--- internal/models/room.go
+++ internal/models/room.go
@@ -2,7 +2,9 @@ package models
 
 // RoomT represents rooms in each building
 type Room struct {
-	RoomID     int    `json:"room_id"`
-	BuildingID int    `json:"building_id"`
-	RoomCode   string `json:"room_code"`
+	RoomID       int    `json:"room_id"`
+	BuildingID   int    `json:"building_id"`
+	RoomCode     string `json:"room_code"`
+	BuildingCode string `json:"building_code"`
+	SiteName     string `json:"site_name"`
 }
