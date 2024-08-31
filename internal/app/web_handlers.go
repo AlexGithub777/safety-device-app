40feb93637a76262e0988e980f8ed2c649973d3c
Alex <alexscott200020@gmail.com>
2024-08-31 22:53:39 +1200

    added admin page

--- internal/app/web_handlers.go
+++ internal/app/web_handlers.go
@@ -15,3 +15,8 @@ func (a *App) HandleHome(c echo.Context) error {
 func (a *App) HandleDashboard(c echo.Context) error {
 	return c.Render(http.StatusOK, "dashboard.html", nil)
 }
+
+// HandleAdmin serves the admin page
+func (a *App) HandleAdmin(c echo.Context) error {
+	return c.Render(http.StatusOK, "admin.html", nil)
+}
