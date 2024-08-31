6ce3b84fd0911da2f8749db27b14ded001517f30
Alex <alexscott200020@gmail.com>
2024-08-31 16:51:06 +1200

    testing working 100%

--- internal/database/queries_test.go
+++ internal/database/queries_test.go
@@ -2,6 +2,7 @@ package database_test
 
 import (
 	"database/sql"
+	"errors"
 	"testing"
 	"time"
 
@@ -15,8 +16,8 @@ func TestFetchAllDevices(t *testing.T) {
 	testCases := []struct {
 		name            string
 		expectedDevices []models.EmergencyDevice
-		nextInspection  time.Time
-		expireDate      time.Time
+		mockSetup       func(mock sqlmock.Sqlmock)
+		expectedError   error
 	}{
 		{
 			name: "TestFetchAllDevices with valid non-null fields",
@@ -32,10 +33,10 @@ func TestFetchAllDevices(t *testing.T) {
 					Description:             sql.NullString{String: "Description of device", Valid: true},
 					Size:                    sql.NullString{String: "10kg", Valid: true},
 					Status:                  sql.NullString{String: "Active", Valid: true},
+					NextInspectionDate:      sql.NullTime{Time: time.Date(2024, time.October, 15, 0, 0, 0, 0, time.UTC), Valid: true},
+					ExpireDate:              sql.NullTime{Time: time.Date(2029, time.August, 1, 0, 0, 0, 0, time.UTC), Valid: true},
 				},
 			},
-			nextInspection: time.Date(2024, time.October, 15, 0, 0, 0, 0, time.UTC),
-			expireDate:     time.Date(2029, time.August, 1, 0, 0, 0, 0, time.UTC),
 		},
 		{
 			name: "TestFetchAllDevices with valid null fields",
@@ -43,41 +44,98 @@ func TestFetchAllDevices(t *testing.T) {
 				{
 					EmergencyDeviceID:       2,
 					EmergencyDeviceTypeName: "TypeB",
-					ExtinguisherTypeName:    sql.NullString{Valid: true, String: "N/A"},
+					ExtinguisherTypeName:    sql.NullString{Valid: false, String: "N/A"},
 					RoomName:                "Room102",
-					SerialNumber:            sql.NullString{Valid: true, String: "N/A"},
-					ManufactureDate:         sql.NullTime{Time: time.Date(0001, time.January, 1, 0, 0, 0, 0, time.UTC), Valid: true},
-					LastInspectionDate:      sql.NullTime{Time: time.Date(0001, time.January, 1, 0, 0, 0, 0, time.UTC), Valid: true},
-					Description:             sql.NullString{Valid: true, String: "N/A"},
-					Size:                    sql.NullString{Valid: true, String: "N/A"},
-					Status:                  sql.NullString{Valid: true, String: "N/A"},
+					SerialNumber:            sql.NullString{Valid: false, String: "N/A"},
+					ManufactureDate:         sql.NullTime{Valid: false},
+					LastInspectionDate:      sql.NullTime{Valid: false},
+					Description:             sql.NullString{Valid: false, String: "N/A"},
+					Size:                    sql.NullString{Valid: false, String: "N/A"},
+					Status:                  sql.NullString{Valid: false, String: "N/A"},
+					NextInspectionDate:      sql.NullTime{Valid: false},
+					ExpireDate:              sql.NullTime{Valid: false},
 				},
 			},
-			nextInspection: time.Date(0001, time.April, 1, 0, 0, 0, 0, time.UTC),
-			expireDate:     time.Date(0006, time.January, 1, 0, 0, 0, 0, time.UTC),
 		},
 		{
-			name: "TestFetchAllDevices with invalid null fields",
+			name: "TestFetchAllDevices with invalid manufacture date",
 			expectedDevices: []models.EmergencyDevice{
 				{
 					EmergencyDeviceID:       3,
 					EmergencyDeviceTypeName: "TypeC",
-					ExtinguisherTypeName:    sql.NullString{Valid: false, String: "N/A"}, // Null extinguisher type name
-					RoomName:                "Room103",
-					SerialNumber:            sql.NullString{Valid: false, String: "N/A"}, // Null serial number
-					ManufactureDate:         sql.NullTime{Time: time.Date(0001, time.January, 1, 0, 0, 0, 0, time.UTC), Valid: true},
-					LastInspectionDate:      sql.NullTime{Time: time.Date(0001, time.January, 1, 0, 0, 0, 0, time.UTC), Valid: true},
-					Description:             sql.NullString{Valid: false, String: "N/A"}, // Null description
-					Size:                    sql.NullString{Valid: false, String: "N/A"}, // Null size
-					Status:                  sql.NullString{Valid: false, String: "N/A"}, // Null status
+					ExtinguisherTypeName:    sql.NullString{String: "ExtinguisherD", Valid: true},
+					RoomName:                "C103",
+					SerialNumber:            sql.NullString{String: "SN456", Valid: true},
+					ManufactureDate:         sql.NullTime{Valid: false},
+					LastInspectionDate:      sql.NullTime{Time: time.Date(2024, time.July, 15, 0, 0, 0, 0, time.UTC), Valid: true},
+					Description:             sql.NullString{String: "Device with invalid manufacture date", Valid: true},
+					Size:                    sql.NullString{String: "5kg", Valid: true},
+					Status:                  sql.NullString{String: "Active", Valid: true},
+					NextInspectionDate:      sql.NullTime{Time: time.Date(2024, time.October, 15, 0, 0, 0, 0, time.UTC), Valid: true},
+					ExpireDate:              sql.NullTime{Valid: false},
 				},
 			},
-			nextInspection: time.Date(0001, time.April, 1, 0, 0, 0, 0, time.UTC),
-			expireDate:     time.Date(0006, time.January, 1, 0, 0, 0, 0, time.UTC),
+		},
+		{
+			name: "TestFetchAllDevices with invalid inspection date",
+			expectedDevices: []models.EmergencyDevice{
+				{
+					EmergencyDeviceID:       4,
+					EmergencyDeviceTypeName: "TypeD",
+					ExtinguisherTypeName:    sql.NullString{String: "ExtinguisherE", Valid: true},
+					RoomName:                "D104",
+					SerialNumber:            sql.NullString{String: "SN789", Valid: true},
+					ManufactureDate:         sql.NullTime{Time: time.Date(2024, time.August, 1, 0, 0, 0, 0, time.UTC), Valid: true},
+					LastInspectionDate:      sql.NullTime{Valid: false},
+					Description:             sql.NullString{String: "Device with invalid inspection date", Valid: true},
+					Size:                    sql.NullString{String: "8kg", Valid: true},
+					Status:                  sql.NullString{String: "Active", Valid: true},
+					NextInspectionDate:      sql.NullTime{Valid: false},
+					ExpireDate:              sql.NullTime{Time: time.Date(2029, time.August, 1, 0, 0, 0, 0, time.UTC), Valid: true},
+				},
+			},
+		},
+
+		{
+			name: "TestFetchAllDevices with query error",
+			mockSetup: func(mock sqlmock.Sqlmock) {
+				mock.ExpectQuery("^SELECT (.+) FROM emergency_deviceT").WillReturnError(errors.New("database error"))
+			},
+			expectedError: errors.New("database error"),
+		},
+		{
+			name: "TestFetchAllDevices with row scan error",
+			mockSetup: func(mock sqlmock.Sqlmock) {
+				rows := sqlmock.NewRows([]string{
+					"emergencydeviceid",
+					"emergencydevicetypename",
+					"extinguishertypename",
+					"roomname",
+					"serialnumber",
+					"manufacturedate",
+					"lastinspectiondate",
+					"description",
+					"size",
+					"status",
+				}).AddRow(
+					"invalid", // This will cause a scan error as it's not an int
+					"TypeA",
+					sql.NullString{String: "ExtinguisherA", Valid: true},
+					"Room101",
+					sql.NullString{String: "SN123", Valid: true},
+					sql.NullTime{Time: time.Now(), Valid: true},
+					sql.NullTime{Time: time.Now(), Valid: true},
+					sql.NullString{String: "Description", Valid: true},
+					sql.NullString{String: "10kg", Valid: true},
+					sql.NullString{String: "Active", Valid: true},
+				)
+				mock.ExpectQuery("^SELECT (.+) FROM emergency_deviceT").WillReturnRows(rows)
+			},
+			expectedError: errors.New("sql: Scan error on column index 0, name \"emergencydeviceid\": converting driver.Value type string (\"invalid\") to a int: invalid syntax"),
 		},
 	}
 
-	for i, tc := range testCases {
+	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			db, mock, err := sqlmock.New()
 			if err != nil {
@@ -87,69 +145,68 @@ func TestFetchAllDevices(t *testing.T) {
 
 			dbInstance := &database.DB{DB: db}
 
-			rows := sqlmock.NewRows([]string{
-				"emergencydeviceid",
-				"emergencydevicetypename",
-				"extinguishertypename",
-				"roomname",
-				"serialnumber",
-				"manufacturedate",
-				"lastinspectiondate",
-				"description",
-				"size",
-				"status",
-			})
+			if tc.mockSetup != nil {
+				tc.mockSetup(mock)
+			} else {
+				rows := sqlmock.NewRows([]string{
+					"emergencydeviceid",
+					"emergencydevicetypename",
+					"extinguishertypename",
+					"roomname",
+					"serialnumber",
+					"manufacturedate",
+					"lastinspectiondate",
+					"description",
+					"size",
+					"status",
+				})
 
-			for _, device := range tc.expectedDevices {
-				if i == 2 { // If it's the third test case (index starts from 0)
+				for _, device := range tc.expectedDevices {
 					rows.AddRow(
 						device.EmergencyDeviceID,
 						device.EmergencyDeviceTypeName,
-						sql.NullString{Valid: false}, // Mock the database to return Valid: false
+						device.ExtinguisherTypeName,
 						device.RoomName,
-						sql.NullString{Valid: false}, // Mock the database to return Valid: false
-						device.ManufactureDate.Time,
-						device.LastInspectionDate.Time,
-						sql.NullString{Valid: false}, // Mock the database to return Valid: false
-						sql.NullString{Valid: false}, // Mock the database to return Valid: false
-						sql.NullString{Valid: false}, // Mock the database to return Valid: false
+						device.SerialNumber,
+						device.ManufactureDate,
+						device.LastInspectionDate,
+						device.Description,
+						device.Size,
+						device.Status,
 					)
-				} else {
-					rows.AddRow(
-						device.EmergencyDeviceID,
-						device.EmergencyDeviceTypeName,
-						device.ExtinguisherTypeName.String,
-						device.RoomName,
-						device.SerialNumber.String,
-						device.ManufactureDate.Time,
-						device.LastInspectionDate.Time,
-						device.Description.String,
-						device.Size.String,
-						device.Status.String,
-					)
+				}
+
+				mock.ExpectQuery("^SELECT (.+) FROM emergency_deviceT").WillReturnRows(rows)
+			}
+
+			actualDevices, err := dbInstance.FetchAllDevices("your_building_code")
+
+			if tc.expectedError != nil {
+				assert.Error(t, err)
+				assert.Contains(t, err.Error(), tc.expectedError.Error())
+				assert.Nil(t, actualDevices)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, len(tc.expectedDevices), len(actualDevices), "Number of devices does not match")
+
+				for i, expectedDevice := range tc.expectedDevices {
+					actualDevice := actualDevices[i]
+					assert.Equal(t, expectedDevice.EmergencyDeviceID, actualDevice.EmergencyDeviceID, "EmergencyDeviceID does not match")
+					assert.Equal(t, expectedDevice.EmergencyDeviceTypeName, actualDevice.EmergencyDeviceTypeName, "EmergencyDeviceTypeName does not match")
+					assert.Equal(t, expectedDevice.ExtinguisherTypeName, actualDevice.ExtinguisherTypeName, "ExtinguisherTypeName does not match")
+					assert.Equal(t, expectedDevice.RoomName, actualDevice.RoomName, "RoomName does not match")
+					assert.Equal(t, expectedDevice.SerialNumber, actualDevice.SerialNumber, "SerialNumber does not match")
+					assert.Equal(t, expectedDevice.ManufactureDate, actualDevice.ManufactureDate, "ManufactureDate does not match")
+					assert.Equal(t, expectedDevice.LastInspectionDate, actualDevice.LastInspectionDate, "LastInspectionDate does not match")
+					assert.Equal(t, expectedDevice.Description, actualDevice.Description, "Description does not match")
+					assert.Equal(t, expectedDevice.Size, actualDevice.Size, "Size does not match")
+					assert.Equal(t, expectedDevice.Status, actualDevice.Status, "Status does not match")
+					assert.Equal(t, expectedDevice.NextInspectionDate, actualDevice.NextInspectionDate, "NextInspectionDate does not match")
+					assert.Equal(t, expectedDevice.ExpireDate, actualDevice.ExpireDate, "ExpireDate does not match")
 				}
 			}
 
-			mock.ExpectQuery("^SELECT (.+) FROM emergency_deviceT").WillReturnRows(rows)
-
-			// Assert Expected NextInspectionDate and ExpireDate for the first device (all fields are not null)
-			tc.expectedDevices[0].NextInspectionDate = sql.NullTime{Time: tc.nextInspection, Valid: true}
-			tc.expectedDevices[0].ExpireDate = sql.NullTime{Time: tc.expireDate, Valid: true}
-
-			actualDevices, err := dbInstance.FetchAllDevices("your_building_code")
-			if err != nil {
-				t.Fatalf("Failed to fetch devices: %v", err)
-			}
-
-			// Assert the expected devices and actual devices are equal
-
-			assert.ElementsMatch(t, tc.expectedDevices, actualDevices)
-
-			// Additional assertions for NextInspectionDate and ExpireDate
-			for i, actualDevice := range actualDevices {
-				assert.Equal(t, tc.expectedDevices[i].NextInspectionDate.Time, actualDevice.NextInspectionDate.Time, "NextInspectionDate does not match")
-				assert.Equal(t, tc.expectedDevices[i].ExpireDate.Time, actualDevice.ExpireDate.Time, "ExpireDate does not match")
-			}
+			assert.NoError(t, mock.ExpectationsWereMet())
 		})
 	}
 }
