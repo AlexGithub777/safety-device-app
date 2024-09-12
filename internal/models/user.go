e9d1e1091931be71790fb3e891e6f70d9bcbf2a9
Alex <alexscott200020@gmail.com>
2024-09-13 00:11:07 +1200

    updated user model

--- internal/models/user.go
+++ internal/models/user.go
@@ -5,4 +5,6 @@ type User struct {
 	UserID   int    `json:"user_id"`
 	Username string `json:"username"`
 	Password string `json:"password"`
+	Email    string `json:"email"`
+	Role     string `json:"role"`
 }
